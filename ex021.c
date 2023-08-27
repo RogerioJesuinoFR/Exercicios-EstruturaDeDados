d7b968d6710ddd155c87d126404acce680c0cb41
Rogerio Jesuino Ferraz Rocha <rogeriojesuino@alunos.utfpr.edu.br>
2023-08-27 13:41:36 -0300

    Update ex021.c

--- ex021.c
+++ ex021.c
@@ -4,22 +4,21 @@ a cada posição do array usando o ponteiro (use *)*/
 #include <stdio.h>
 
 int main() {
-    int array[5] = {1, 2, 3, 4, 5}; // Declare um array de inteiros com 5 elementos.
-    int *p = array; // Associe o ponteiro ao array.
+    int array[5] = {1, 2, 3, 4, 5};
+    int *p = array;
 
     printf("Array atual:\n");
     for (int i = 0; i < 5; i++) {
         printf("%d ", array[i]);
     }
     printf("\n\n");
-    // Use um loop para adicionar 1 a cada posição do array usando o ponteiro.
+
     for (int i = 0; i < 5; i++) {
-        (*p)++; // Adicione 1 à posição atual do array através do ponteiro.
-        p++; // Mova o ponteiro para a próxima posição do array.
+        (*p)++;
+        p++;
     }
 
     printf("Array adicionado 1:\n");
-    // Agora, imprima o array para verificar as mudanças.
     for (int i = 0; i < 5; i++) {
         printf("%d ", array[i]);
     }
